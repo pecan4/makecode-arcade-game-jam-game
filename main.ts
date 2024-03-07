52ed919171fdb1db89747063ccdc405ab564fd30
pecan4 <157906054+pecan4@users.noreply.github.com>
2024-03-06 21:09:58 -0500

    color palette changed, some art made, major art push

--- main.ts
+++ main.ts
@@ -1,12 +1,3 @@
 namespace SpriteKind {
     export const tet_box_image = SpriteKind.create()
 }
-let tile_map_floors = [
-tilemap`plus floor`,
-tilemap`level8`
-]
-let myMenu = miniMenu.createMenu(
-miniMenu.createMenuItem("abc"),
-miniMenu.createMenuItem("abc"),
-miniMenu.createMenuItem("abc")
-)
